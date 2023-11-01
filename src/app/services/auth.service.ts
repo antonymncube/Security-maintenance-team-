936de89e12fccb6a12299a890fd3e8844b3c2b3d
Emihle Ntshongwana <Ntshongwanae@gmail.com>
2023-11-01 09:46:27 +0200

    testing team work

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,46 +1,60 @@
+import { AuthGuard } from './../auth.guard';
 import { Injectable } from '@angular/core';
 import { ApiServiceService } from './api-service.service';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private isAuthenticated = false;
+  private isAuthenticated$ = new BehaviorSubject<boolean>(false);
   private users: any[] = [];
-  currentuser : string= ''
+  currentuser: string = '';
 
-  constructor(private apiservice: ApiServiceService,private router : Router) {}
+  constructor(private apiservice: ApiServiceService, private router: Router) {
+    this.checkAuthenticationStatus();
+  }
 
-  login(username: string, password: string): boolean {
- 
-    this.apiservice.getData().subscribe((data: any) => {
-      this.users = data;
-      console.log(username)
-       
-      const authenticatedUser = this.users.find(user =>  user.username === username && user.password === password);
+  login(username: string, password: string): Promise<boolean> {
+    return new Promise<boolean>((resolve) => {
+      this.apiservice.getData().subscribe((data: any) => {
+        this.users = data;
 
-      if (authenticatedUser) {
-        this.isAuthenticated = true;
-      }
+        const authenticatedUser = this.users.find(user => user.username === username && user.password === password);
+
+        if (authenticatedUser) {
+          this.isAuthenticated$.next(true);
+          this.setCurrentUser(username);
+          resolve(true);
+        } else {
+          this.isAuthenticated$.next(false);
+          resolve(false);
+        }
+      });
     });
-
-    return this.isAuthenticated;
   }
 
   logout(): void {
-    this.isAuthenticated = false;
-    this.router.navigate(['/login']); // Navigate to the login page's route
+    this.isAuthenticated$.next(false);
+    this.router.navigate(['/login']);
   }
 
-  isAuthenticatedUser(): boolean {
-    return this.isAuthenticated;
+  isAuthenticatedUser(): BehaviorSubject<boolean> {
+    return this.isAuthenticated$;
   }
+
   setCurrentUser(username: string): void {
     this.currentuser = username;
-    sessionStorage.setItem('currentuser', username)
+    sessionStorage.setItem('currentuser', username);
   }
-  
+
+  checkAuthenticationStatus(): void {
+    const username = sessionStorage.getItem('currentuser');
+    if (username) {
+      this.isAuthenticated$.next(true);
+      this.currentuser = username;
+    }
+  }
 }
-
-
