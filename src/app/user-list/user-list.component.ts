f8ff4464cb30c09ca2e082319097d61ad000f45c
Emihle Ntshongwana <Ntshongwanae@gmail.com>
2023-11-22 14:30:50 +0200

    modified:   SecurityMaintenanceDB.json 	modified:   src/app/services/api-service.service.ts 	modified:   src/app/user-list/user-list.component.html 	modified:   src/app/user-list/user-list.component.ts

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -73,16 +73,20 @@ export class UserListComponent implements OnInit {
       this.router.navigate(['/home/viewuser', element.id]);
     } else if (action === 'toggleStatus') {
       this.toggleUserStatus(element.id);
+    } else if (action === 'delete') {
+      // this.onDeleteUser(element.id);
     }
   }
 
-  // onDeleteUser(userId: string): void {
+  // onDeleteUser(userId: number): void {
   //   if (confirm('Are you sure you want to delete this user?')) {
-  //     this.apiService.deleteUser(userId).subscribe(() => {
-
-  //         location.reload();
-  //       });
-  //     }}
+  //     this.apiService.deleteUser(userId.toString()).subscribe(() => {
+  //       // You might not need to reload the location, as Angular's change detection will update the UI.
+  //       // However, if needed, you can force a reload using window.location.reload()
+  //       location.reload();
+  //     });
+  //   }
+  // }
 
 
 
@@ -105,7 +109,7 @@ export class UserListComponent implements OnInit {
         if (status === null || status === '') {
           // If status is null, no filter should be applied
           this.dataSource.filter = '';
-          
+
         } else {
           // Otherwise, apply the filter based on the boolean value
           this.dataSource.filter = status === 'true' ? 'true' : 'false';
