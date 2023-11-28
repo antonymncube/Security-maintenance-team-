016e702e920be1470fc9336bb766c2580e1accb6
ApheleleRana <ranaaphelele@gmail.com>
2023-11-28 10:25:17 +0200

    responsive nav v1.2.4

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -11,6 +11,7 @@ import { ApiServiceService } from '../services/api-service.service';
 import { AccessGroupComponent } from '../access-group/access-group.component';
 import { Router } from '@angular/router';
 import { navbarData } from './narbardata';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 interface SideNavToggle{
   screenWidth: number;
@@ -20,7 +21,30 @@ interface SideNavToggle{
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('350ms',
+        style({opacity: 1}))
+      ]),
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('3ms',
+        style({opacity: 0}))
+      ])
+    ]),
+    trigger('rotate',[
+      transition(':enter', [
+        animate('1000ms',
+        keyframes([
+          style({transform: 'rotate(0deg)', offset: '0'}),
+          style({transform: 'rotate(2turn)', offset: '1'})
+        ]))
+      ])
+    ])
+  ]
 })
 export class NavBarComponent {
   private breakpointObserver = inject(BreakpointObserver);
